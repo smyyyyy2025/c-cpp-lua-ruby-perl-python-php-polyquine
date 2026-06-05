67eba6c101b4a6c56c2c01f36f18a9057b1e68ec
smyyyyy <1298311250@qq.com>
2026-06-05 09:04:45 +0800

    Add files via upload

---
+++ polyquine.c
@@ -0,0 +1,10 @@
+#include<stdio.h>/*
+type(#'');printf=function(f,...)io.write(f:format(...))end--[[
+1);s=1;s//1#s/;BEGIN{class Integer def/(_)end end;def type(a)end};a=<<E
+'''=;BEGIN{sub type{}}sub v:lvalue{$v};$a=<<E;
+*/
+#define type(v) }
+int main(){char*v;/*
+E
+#''';printf=lambda f,*a:print(f%a,end='')#*///]]
+v="#include<stdio.h>/*%ctype(#'');printf=function(f,...)io.write(f:format(...))end--[[%c1);s=1;s//1#s/;BEGIN{class Integer def/(_)end end;def type(a)end};a=<<E%c'''=;BEGIN{sub type{}}sub v:lvalue{%cv};%ca=<<E;%c*/%c#define type(v) }%cint main(){char*v;/*%cE%c#''';printf=lambda f,*a:print(f%%a,end='')#*///]]%cv=%c%s%c;printf(v,10,10,10,36,36,10,10,10,10,10,10,34,v,34);type(v)";printf(v,10,10,10,36,36,10,10,10,10,10,10,34,v,34);type(v)
