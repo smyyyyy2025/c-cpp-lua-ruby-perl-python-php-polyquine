cc46acdd890443accba4f188e4764f2a12505c0d
smyyyyy <1298311250@qq.com>
2026-06-07 00:32:05 +0800

    Update polyquine.c

--- polyquine.c
+++ polyquine.c
@@ -1,10 +1,10 @@
 #include<stdio.h>/*
 type(#'');printf=function(f,...)io.write(f:format(...))end--[[
-1);s=1;s//1#s/;BEGIN{class Integer def/(_)end end;def type(a)end};a=<<E
-'''=;BEGIN{sub type{}}sub v:lvalue{$v};$a=<<E;
+1);s=1;s//1#s/;BEGIN{class Integer def/(_)end end;def type(a)end};<<E
+'''==;BEGIN{sub type{}}sub v:lvalue{$v};<<E;
 */
 #define type(v) }
 int main(){char*v;/*
 E
 #''';printf=lambda f,*a:print(f%a,end='')#*///]]
-v="#include<stdio.h>/*%ctype(#'');printf=function(f,...)io.write(f:format(...))end--[[%c1);s=1;s//1#s/;BEGIN{class Integer def/(_)end end;def type(a)end};a=<<E%c'''=;BEGIN{sub type{}}sub v:lvalue{%cv};%ca=<<E;%c*/%c#define type(v) }%cint main(){char*v;/*%cE%c#''';printf=lambda f,*a:print(f%%a,end='')#*///]]%cv=%c%s%c;printf(v,10,10,10,36,36,10,10,10,10,10,10,34,v,34);type(v)";printf(v,10,10,10,36,36,10,10,10,10,10,10,34,v,34);type(v)
+v="#include<stdio.h>/*%ctype(#'');printf=function(f,...)io.write(f:format(...))end--[[%c1);s=1;s//1#s/;BEGIN{class Integer def/(_)end end;def type(a)end};<<E%c'''==;BEGIN{sub type{}}sub v:lvalue{%cv};<<E;%c*/%c#define type(v) }%cint main(){char*v;/*%cE%c#''';printf=lambda f,*a:print(f%%a,end='')#*///]]%cv=%c%s%c;printf(v,10,10,10,36,10,10,10,10,10,10,34,v,34);type(v)";printf(v,10,10,10,36,10,10,10,10,10,10,34,v,34);type(v)
