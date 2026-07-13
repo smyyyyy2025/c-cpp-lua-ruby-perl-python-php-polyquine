e5ff252e800bf0aa76dbf0777bd216c59332e4b4
smyyyyy <1298311250@qq.com>
2026-07-14 02:42:40 +0800

    swap to 476-byte 5-line version (pp35 approach)

--- polyquine.py
+++ polyquine.py
@@ -1,6 +1,5 @@
 #include/*
-m=(#[[]
-);'''*/<stdio.h>
-int main(){char*
-#define printf(_...)printf(_);}//'#=;sub _:lvalue{$_}#''';printf=lambda f,*a:print(end=f%a)#]])printf=function(f,...)io.write(f:format(...))end
-_="#include/*%cm=(#[[]%c);'''*/<stdio.h>%cint main(){char*%c#define printf(_...)printf(_);}//'#=;sub _:lvalue{%c_}#''';printf=lambda f,*a:print(end=f%%a)#]])printf=function(f,...)io.write(f:format(...))end%c_=%c%s%c;printf(_,10,10,10,10,36,10,34,_,34)";printf(_,10,10,10,10,36,10,34,_,34)
+m=[['''*/<stdio.h>
+char*
+#define printf(_...)int main(){printf(_);}//]=;sub _:lvalue{$_}#]]printf=function(f,...)io.write(f:format(...))end--']]#''']];printf=lambda f,*a:print(end=f%a)
+_="#include/*%cm=[['''*/<stdio.h>%cchar*%c#define printf(_...)int main(){printf(_);}//]=;sub _:lvalue{%c_}#]]printf=function(f,...)io.write(f:format(...))end--']]#''']];printf=lambda f,*a:print(end=f%%a)%c_=%c%s%c;printf(_,10,10,10,36,10,34,_,34)";printf(_,10,10,10,36,10,34,_,34)
