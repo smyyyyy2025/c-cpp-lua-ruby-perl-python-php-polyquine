00300ba6cef15c3b729230a452f8f56492c6539f
smyyyyy <smyyyyy@smyyyyydeMacBook-Air.local>
2026-07-14 02:36:00 +0800

    add quine_pp35: 7-language polyquine (C/C++/Lua/Ruby/Perl/Python/PHP)

---
+++ quine_pp35.c
@@ -0,0 +1,5 @@
+#include/*
+m=[['''*/<stdio.h>
+char*
+#define printf(_...)int main(){printf(_);}//]=;sub _:lvalue{$_}#]]printf=function(f,...)io.write(f:format(...))end--']]#''']];printf=lambda f,*a:print(end=f%a)
+_="#include/*%cm=[['''*/<stdio.h>%cchar*%c#define printf(_...)int main(){printf(_);}//]=;sub _:lvalue{%c_}#]]printf=function(f,...)io.write(f:format(...))end--']]#''']];printf=lambda f,*a:print(end=f%%a)%c_=%c%s%c;printf(_,10,10,10,36,10,34,_,34)";printf(_,10,10,10,36,10,34,_,34)
